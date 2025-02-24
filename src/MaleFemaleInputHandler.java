f8ef12b93559d90f50bad6a7fb35b2154fe4c6c8
SidneyTing <sidney_ting@dlsu.edu.ph>
2025-02-24 12:05:29 +0800

    refactor: added input handler for M/F

---
+++ src/MaleFemaleInputHandler.java
@@ -0,0 +1,14 @@
+public class MaleFemaleInputHandler extends AbstractInputHandler {
+    @Override
+    protected boolean validate(String input) {
+        if (input.isEmpty()) {
+            return false;
+        }
+        return input.substring(0, 1).equalsIgnoreCase("M") || input.substring(0, 1).equalsIgnoreCase("F");
+    }
+
+    @Override
+    protected void displayInvalidMessage() {
+        System.out.println("Invalid input. Please enter \"M\" or \"F\".");
+    }
+}
